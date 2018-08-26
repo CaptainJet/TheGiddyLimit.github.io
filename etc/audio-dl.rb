3ca374713232a7f11c406893a7a81dd774befa38
Robert Rowe <1669080+CaptainJet@users.noreply.github.com>
2018-08-25 22:24:01 -0700

    Mild utility script improvement?

--- etc/audio-dl.rb
+++ etc/audio-dl.rb
@@ -1,13 +1,16 @@
 require 'json'
 files = Dir["data/bestiary/bestiary-*.json"]
+all_clips = []
 files.each do |a|
 	json = JSON.parse(File.read(a))
 	sclips = json['monster'].collect do |b| b["soundClip"] end
 	sclips.delete('""')
 	sclips.compact!
-	sclips.each do |c| `wget -N #{c} -P ./audio/bestiary/` end
+	all_clips.concat(sclips)
 end
-
+all_clips.uniq!
+all_clips.each do |c| `wget -N #{c} -P ./audio/bestiary/` end
+	
 json = JSON.parse(File.read("data/races.json"))
 sclips = json['race'].collect do |b| b["soundClip"] end
 sclips.delete('""')
